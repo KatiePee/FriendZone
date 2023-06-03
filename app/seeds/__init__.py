92dadf72707b9c2c9f676b74cf1f47a47744eb4e
Prabhjot Singh <pjsingh1359@gmail.com>
2023-06-03 16:47:02 -0700

    add seeders to post images

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -2,6 +2,7 @@ from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .posts import seed_posts, undo_posts
 from .likes import seed_likes, undo_likes
+from .post_images import seed_post_images, undo_post_images
 
 from app.models.db import db, environment, SCHEMA
 
@@ -18,18 +19,21 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_post_images()
         undo_likes()
         undo_posts()
         undo_users()
     seed_users()
     seed_posts()
     seed_likes()
+    seed_post_images()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_post_images()
     undo_likes()
     undo_posts()
     undo_users()
