d17f65f2862f2275fba3dea6dc3910469c1f1dd7
Prabhjot Singh <pjsingh1359@gmail.com>
2023-06-04 12:27:25 -0700

    got friendship seeder working

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -21,7 +21,7 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        # undo_friendships()
+        undo_friendships()
         undo_comments()
         undo_post_images()
         undo_likes()
@@ -32,14 +32,14 @@ def seed():
     seed_likes()
     seed_post_images()
     seed_comments()
-    # seed_friendships()
+    seed_friendships()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    # undo_friendships()
+    undo_friendships()
     undo_comments()
     undo_post_images()
     undo_likes()
