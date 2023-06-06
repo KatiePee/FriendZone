379281c4784e91978e58853a01f93d20a9f8c593
Albert <Alberthsk115@gmail.com>
2023-06-05 22:03:51 -0700

    added close modal

--- react-app/src/components/PostDetailModal/index.js
+++ react-app/src/components/PostDetailModal/index.js
@@ -1,26 +1,38 @@
-import React, { useEffect, useState } from "react"
-import { useSelector } from "react-redux"
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useModal } from "../../context/Modal"
 // import "./postcard.css"
+import "./postdetailmodal.css"
+
 
 function PostDetailModal({ post }) {
-  const { id, content, numLikes, author, postImages, likedBy, comments, createdAt } = post
-  const { firstName, lastName, profilePicURL } = author
-  const user = useSelector(state => state.session.user)
-  console.log("THIS IS THE COMMENTS", comments)
-  const [text, setText] = useState("")
+  const {
+    id,
+    content,
+    numLikes,
+    author,
+    postImages,
+    likedBy,
+    comments,
+    createdAt,
+  } = post;
+  const { firstName, lastName, profilePicURL } = author;
+  const user = useSelector((state) => state.session.user);
+  const [text, setText] = useState("");
+  const { closeModal } = useModal()
 
   const handleInputChange = (e) => {
     setText(e.target.value);
-  }
+  };
 
   const textareaStyle = {
-    resize: 'none',
-    overflow: 'hidden'
+    resize: "none",
+    overflow: "hidden",
   };
 
   const timeAgo = (dateObj) => {
     const date = new Date(dateObj);
-    const currentDate = new Date()
+    const currentDate = new Date();
     const timeDif = currentDate.getTime() - date.getTime();
 
     const seconds = Math.floor(timeDif / 1000);
@@ -30,34 +42,39 @@ function PostDetailModal({ post }) {
     const months = Math.floor(days / 30);
     const years = Math.floor(months / 12);
 
-    const month = date.toLocaleString('default', { month: 'long' });
-    const year = date.toLocaleString('default', { year: 'numeric' });
-    const options = { hour: 'numeric', minute: 'numeric', hour12: true };
-    const time = date.toLocaleString('en-US', options);
-    const dateString = `${month} ${year} at ${time}`
+    const month = date.toLocaleString("default", { month: "long" });
+    const year = date.toLocaleString("default", { year: "numeric" });
+    const options = { hour: "numeric", minute: "numeric", hour12: true };
+    const time = date.toLocaleString("en-US", options);
+    const dateString = `${month} ${year} at ${time}`;
 
-    if (days >= 7) return dateString
-    else if (days >= 1) return `${days}d`
-    else if (hours >= 1) return `${hours}d`
-    else return minutes ? `${minutes}m` : '1m'
-
-  }
+    if (days >= 7) return dateString;
+    else if (days >= 1) return `${days}d`;
+    else if (hours >= 1) return `${hours}d`;
+    else return minutes ? `${minutes}m` : "1m";
+  };
 
   return (
     <div className="post-card__container">
       <div className="post-card__info-content">
         <div className="post-card__user-info">
           <div className="post-card__profile-info">
-            <img className="post-card__profile-pic" src={profilePicURL} alt="profile" />
+            <img
+              className="post-card__profile-pic"
+              src={profilePicURL}
+              alt="profile"
+            />
             <div className="profile-info__left-side">
-              <p>{firstName} {lastName}</p>
+              <p>
+                {firstName} {lastName}
+              </p>
               <div className="profile-info__post-date">
                 <span>{timeAgo(createdAt)}</span>
               </div>
             </div>
           </div>
           <div className="post-card__edit-delete">
-            <span>Close</span>
+            <div className="close-div" onClick={closeModal}>Close</div>
           </div>
         </div>
         <div className="post-card__content">
@@ -65,30 +82,59 @@ function PostDetailModal({ post }) {
         </div>
       </div>
       <div className="post-card__images">
-        {postImages.map(image => {
-          return <img src={image.imageUrl} />
+        {postImages.map((image) => {
+          return <img src={image.imageUrl} />;
         })}
       </div>
       <div className="post-card__details">
-        <div className="post-card__engagement">{numLikes <= 0 ? "" : `❤ ${numLikes}` }</div>
+        <div className="post-card__engagement">
+          {numLikes <= 0 ? "" : `❤ ${numLikes}`}
+        </div>
         <div className="post-card__buttons">
           <span>LIKE</span>
         </div>
         <div>
-            {comments.map((comment) => (
-                <div>{comment.userId} {comment.content}</div>
-            ))}
+          {comments.map((comment) => (
+            <>
+              <div className="post-card__profile-info">
+                <img
+                  className="post-card__profile-pic"
+                  src={comment.commentAuthor.profilePicURL}
+                  alt="profile pic"
+                />
+                <div className="profile-info__left-side">
+                  <p>
+                    {comment.commentAuthor.firstName}{" "}
+                    {comment.commentAuthor.lastName}
+                  </p>
+                </div>
+              </div>
+              <div className="post-card__comment-content">
+                {" "}
+                {comment.content}{" "}
+              </div>
+            </>
+          ))}
         </div>
         <div className="post-card__comment-bar">
-          <img className="post-card__profile-pic" src={user.profilePicURL} alt="profile" />
+          <img
+            className="post-card__profile-pic"
+            src={user.profilePicURL}
+            alt="profile"
+          />
           <div>
-            <textarea className="add-comment" value={text} onChange={handleInputChange} rows={1}></textarea>
+            <textarea
+              className="add-comment"
+              value={text}
+              onChange={handleInputChange}
+              rows={1}
+            ></textarea>
             <span>➡</span>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default PostDetailModal
+export default PostDetailModal;
