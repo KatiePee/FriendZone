a4026707e76e17b2a4b4a783c0b1f6c17dde95ef
Albert <Alberthsk115@gmail.com>
2023-06-07 14:00:42 -0700

    got landing page to work

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -6,17 +6,22 @@ import OpenModalButton from '../OpenModalButton'
 import "./homepage.css"
 import PostFormModal from "../PostFormModal";
 import CreatePost from "../CreatePost/CreatePost";
+import { Redirect } from "react-router-dom";
+
 
 function HomePage() {
   const dispatch = useDispatch();
   const postsState = useSelector(state => state.posts.allPosts)
   const user = useSelector(state => state.session.user)
   const posts = postsState ? Object.values(postsState).reverse() : [];
-  const { firstName, profilePicURL } = user
 
   useEffect(() => {
     dispatch(allPostsThunk());
   }, [dispatch])
+
+  if (!user) {
+    return <Redirect to='/'/>
+  }
 
   if (!posts.length) return null;
   // return (<h1>HomePage!</h1>)
