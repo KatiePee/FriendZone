ef7435d9135908b1d22d0fe71a61441306d1de2b
Albert <Alberthsk115@gmail.com>
2023-06-08 21:48:56 -0700

    added nonexistent page

---
+++ react-app/src/components/NonExistentPage/index.js
@@ -0,0 +1,26 @@
+import { useHistory } from "react-router-dom"
+
+
+function NonExistent() {
+    const history = useHistory()
+
+    const redirect = async (e) => {
+        history.push('/home')
+        return
+    }
+
+
+
+    return (
+        <div>
+            <h1>INSERT A LOCK IMAGE HERE</h1>
+            <h2>This content isn't available right now</h2>
+            <br><h3>When this happens, it's usually because you tried to manually input,</h3></br>
+            <br><h3>A route that doesn't exist. Don't try that again, you SMUCK!</h3></br>
+            <button onClick={redirect}>Go to News Feed</button>
+        </div>
+    )
+}
+
+
+export default NonExistent;
